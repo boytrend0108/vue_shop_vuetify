78145cd80316866abeba5c7cb516848a1bc110f8
boytrend <boytrend@gmail.com>
2023-02-12 13:18:46 +0400

    made form

--- src/pages/Login/P-Login.vue
+++ src/pages/Login/P-Login.vue
@@ -1,14 +1,20 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xl12 class="w-100">
-        <h1>Login</h1>
-      </v-flex>
-    </v-layout>
+  <v-container >
+    <v-row class="mt-10">
+      <v-col class="d-flex justify-center">
+       <login-form class="w-50" />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
+
 <script >
+import loginForm from './components/C-Login'
 export default{
+  components:{
+    loginForm
+  },
+
    data(){
     return{}
    }
---
+++ src/pages/Login/components/C-Login.vue
@@ -0,0 +1,83 @@
+<template>
+  <div >
+    <v-card class="mx-auto px-6 py-8 bg-primary" max-width="500">
+      <v-form
+        v-model="form"
+        @submit.prevent="onSubmit"
+        class=""
+      >
+        <v-text-field
+          v-model="email"
+          :readonly="loading"
+          :rules="emailRules"
+          class="mb-2"
+          bg-color="white"
+          clearable
+          label="Email"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="password"
+          :readonly="loading"
+          :rules="passwordRules"
+          clearable
+          counter="6"
+          bg-color="white"
+          label="Password"
+          placeholder="Enter your password"
+        ></v-text-field>
+        <br>
+        <v-btn
+          :disabled="!form"
+          :loading="loading"
+          color="error"
+          size="large"
+          type="submit"
+        >
+          Sign In
+        </v-btn>
+      </v-form>
+    </v-card>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      form: false,
+      email: null,
+      password: null,
+      loading: false,
+      emailRules: [
+        value => {
+          if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
+          return 'Must be a valid e-mail.'
+        },
+      ],
+      passwordRules: [
+        value => {
+          if (value?.length > 6) return true
+          return 'Password needs to be at least 6 symbols.'
+        },
+      ],
+    }),
+
+    methods: {
+      onSubmit () {
+        if (!this.form) return
+
+        this.loading = true
+
+        setTimeout(() => (this.loading = false), 2000)
+      },
+      required (v) {
+        return !!v || 'Field is required'
+      },
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
